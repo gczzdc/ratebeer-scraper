14bd333ee663a7cefe118a236fd97d1e41d77ef5
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-09-19 13:44:24 -0700

    added selenium for single-threaded requests

--- scraper.py
+++ scraper.py
@@ -1,15 +1,33 @@
 import requests
 
+import os  
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options  
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+
+
+
+
 max_errors = 5
 
-def scrape_one(page, max_errors = max_errors):
+def scrape_one(page, max_errors = max_errors, use_selenium = False):
 	recent_errors = 0
-	while recent_errors < max_errors:
-		response = requests.get(page)
-		if response.status_code ==200:
-			return (response.text)
+	if use_selenium:
+		#note, this has no validation of html status codes (or anything else)
+		driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
+		response = driver.get(page)
+		return(response.page_source)
 	else:
-		raise Exception("too many errors; last status"+ str(response.status_code) + '; text: '+response.text )
+		while recent_errors < max_errors:
+			response = requests.get(page)
+			if response.status_code ==200:
+				return (response.text)
+		else:
+			raise Exception("too many errors; last status"+ str(response.status_code) + '; text: '+response.text )
 
 def scrape_many(pages):
 	pass
