fixed averaging (ibus was miscounting nans; average was not using timedeltas but absolute times)

ratebeer_scraper.py
@@ -104,16 +104,22 @@ def generate_all(delay = delay, loud=True):
 		all_beers.extend(beers)
 	beer_length = len(all_beers)
 	has_ibu = 0
+	total_time = 0
+	non_local = 0
 	driver = scraper.start_driver() #need javascript for these pages
 	for (j,beer) in enumerate(all_beers):
 		t0 = time.time()
 		beer_file = 'beers/'+clean_address_for_filename(beer)+'.pickle'
 		beer_data, local = check_local(beer_file, scrape_and_parse_beer, [beer,driver], delay)
-		if beer_data['ibu'] != np.nan:
+		if beer_data and (not np.isnan(beer_data['ibu'])):
 			has_ibu+=1
 		if loud and not local:
-			print ('completed beer',j, 'of', beer_length,'in',round(time.time()-t0,2),'seconds')		
+			non_local +=1
+			t1 = time.time()
+			total_time+=t1-t0
+			print ('completed beer',j, 'of', beer_length,'in',round(t1-t0,2),'seconds')		
 			print (has_ibu,'beers with ibu data (ratio',round(has_ibu/(j+1) , 3),')')
+			print ('average time per beer',round(total_time/non_local,3))
 	driver.close()
 
 def find_regions(regions_page = regions_page):